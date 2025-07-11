544e7adcabc4d5754cf19ce718a9219f3b2120e9
Marshall <marshall@pop-os.tailec2269.ts.net>
2025-07-11 14:35:20 +0100

    Added Prisma and schema

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const express = require("express")
 console.log("Hello");
+
+const app = express()
+
+const PORT = 1231
+
+app.listen(PORT, "0.0.0.0", () => {
+	console.log(`Listening on ${PORT}`)
+})
